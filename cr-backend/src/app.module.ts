8e94fe79aab9df41f4786977ac5e192e6f348c89
natto <natt@nattos-MacBook-Pro.local>
2020-11-06 23:28:28 +0700

    uses CourseService for finding courses on backend

--- cr-backend/src/app.module.ts
+++ cr-backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CoursesController } from './courses.controller';
+import { CoursesService } from './courses.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoursesController],
-  providers: [AppService],
+  providers: [AppService, CoursesService],
 })
 export class AppModule {}
